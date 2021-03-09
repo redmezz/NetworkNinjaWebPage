5ed494efc7dcf4d865e9f986d7878eff68802506
AdevRamsey <48955764+redmezz@users.noreply.github.com>
2021-03-08 18:36:13 -0700

    Splashplay prog

---
+++ PHP/connect.php
@@ -0,0 +1,16 @@
+<?php
+
+$db = mysqli_connect('localhost','username', 'password','database_name') or
+die('Error connecting to the MySQL server.');
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <h1>PHP connect to MySQL</h1>
+   </body>
+ </html>
